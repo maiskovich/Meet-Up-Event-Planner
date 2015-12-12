758f702531cdc7bc14c5b3cbb331547eb8fefe1d
Matias Aiskovich <matias.aiskovich@caretronics.com.ar>
2015-12-12 15:53:52 -0300

    Adds event starts and ends hours based on a start date and a duration for the event.
    Adds a guest list.

--- src/app/events/events.controller.js
+++ src/app/events/events.controller.js
@@ -5,9 +5,36 @@
     .controller('EventsController', EventsController);
 
   /** @ngInject */
-  function EventsController($scope, $firebaseObject, $firebaseArray, firebaseUrl, Auth, $state) {
+  function EventsController($scope, $firebaseObject, $firebaseArray, firebaseUrl, Auth, $state, $log) {
     var vm = this;
     var $ref = new Firebase(firebaseUrl);
+
+    var d = new Date();
+    d.setHours(0);
+    d.setMinutes(0);
+    $scope.duration = d;
+    $scope.$watchGroup(['start', 'duration'], function (newValue, oldValue) {
+      var startDate = new Date($scope.start);
+      startDate.setHours(startDate.getHours() + $scope.duration.getHours());
+      startDate.setMinutes(startDate.getMinutes() + $scope.duration.getMinutes());
+
+      $scope.finish = startDate;
+    });
+
+    // Item List Arrays
+    $scope.guests = [];
+
+    $scope.addGuest = function () {
+      $scope.guests.push($scope.guest);
+      $scope.guest = '';
+      $log.log($scope.guests);
+    };
+    $scope.removeGuest = function (guest) {
+      var index = $scope.guests.indexOf(guest);
+      $scope.guests.splice(index, 1);
+    }
+
+
   }
 
 
