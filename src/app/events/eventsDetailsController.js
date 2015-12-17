9588bd7c155e863a0ab0764bcb229b88b6700b7b
Matias Aiskovich <matias.aiskovich@caretronics.com.ar>
2015-12-16 21:18:29 -0300

    Adds second password input to the form for register an user, to verify if the passwords match

--- src/app/events/eventsDetailsController.js
+++ src/app/events/eventsDetailsController.js
@@ -5,11 +5,22 @@
     .controller('EventsDetailsController', EventsDetailsController);
 
   /** @ngInject */
-  function EventsDetailsController($scope, $firebaseArray, firebaseUrl, $stateParams, $log) {
+  function EventsDetailsController($scope, $firebaseObject, $firebaseArray, firebaseUrl, $stateParams, $log) {
     var vm = this;
     var $ref = new Firebase(firebaseUrl);
+    var key = $stateParams.key;
     // create a synchronized array
-    $scope.eventsArray = $firebaseArray($ref);
+    $scope.data = $firebaseObject($ref);
+    $ref.on("value", function (snapshot) {
+      console.log(snapshot.val().key);
+    }, function (errorObject) {
+      console.log("The read failed: " + errorObject.code);
+    });
+    $log.log($scope.data);
+
+
+
+
     // Item List Arrays
     $scope.guests = [];
 
